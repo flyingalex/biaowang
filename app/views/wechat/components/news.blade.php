4d7aeddfb709ef787fb56937aaea88ba395515cf
hyy <376246826@qq.com>
2015-11-27 13:12:00 +0800

    Fix:新闻轮播模块

--- app/views/wechat/components/news.blade.php
+++ app/views/wechat/components/news.blade.php
@@ -1,10 +1,12 @@
-<div class="news-container">
-    @if( isset( $news ) )
-        @foreach( $news as $new )
-    <div class="news-item">
-        <img src="/images/icon/news.png" class="news-icon">
-        <span class="news-content">{{$new->content}}</span>
+<div class="swiper-container news-container">
+    <div class="swiper-wrapper">
+        @if( isset( $news ) )
+            @foreach( $news as $new )
+        <div class="news-item swiper-slide">
+            <img src="/images/icon/news.png" class="news-icon">
+            <span class="news-content">{{$new->content}}</span>
+        </div>
+            @endforeach
+        @endif
     </div>
-        @endforeach
-    @endif
 </div>
