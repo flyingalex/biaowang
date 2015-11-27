693d06b80afad81674d6461ac9ab178151cae827
flyingalex <hulinalex@163.com>
2015-11-27 21:42:30 +0800

    添加图片上传逻辑

--- app/controllers/admin/official/AdvertController.php
+++ app/controllers/admin/official/AdvertController.php
@@ -21,6 +21,57 @@ class AdvertController extends Basecontroller{
 		return View::make('admin.pages.official.advert.manage-advert')->with(['adverts'=>$adverts,'activity_adverts'=>$activity_adverts]);
 	}
 
+	// public function createAndEdit()
+	// {	
+	// 	if( Input::has( 'advert_id') )
+	// 	{
+	// 		$advert = Advertisement::find( Input::get('advert_id') );
+	// 		if( !isset( $advert ))
+	// 			return Response::json( BiaoException::$notExist );
+	// 	}else{
+	// 		$advert = new Advertisement;
+	// 	}
+	// 	$file 		= Input::file('image');
+	// 	$title 		= Input::get('title');
+	// 	$sequence 	= Input::get('sequence');
+	// 	// $sequence = 0;
+	// 	$url 		= Input::get('url');
+	// 	$type 		= Input::get('type');//1=微官网广告,2=微投票广告，3=微相册广告
+	// 	// $type = 2;
+	// 	//讲照片存入public目录
+	// 	$path = public_path().'/upload/official/';
+		
+
+	// 	$fullArr = array( $file,$title,$url,$type );
+	// 	$littleArr = array( $title,$url,$type);
+	// 	$dataPath = '/upload/official/';
+	// 	$result = FileController::isFileUpload($advert,$file,$fullArr,$littleArr,$path,$dataPath);
+	// 	if( $result != 'true' )
+	// 		return $result;
+
+	// 	//序号唯一性检查
+	// 	if( !is_numeric( $sequence ) ){
+	// 		return Response::json( BiaoException::$isNotInt );
+	// 	}else{
+	// 		$sequences = Advertisement::where('type',$type)->select('id','sequence')->get();
+	// 		if( InputController::isNotUnique($advert->id,$sequence,$sequences ) )
+	// 			return Response::json( BiaoException::$isNotUnique );
+	// 	}
+	// 	//广告类型
+	// 	if( $type != 1 && $type != 2 && $type != 3)
+	// 		return Response::json( BiaoException::$advertTypeErr );
+
+	// 	if( empty( $sequence ) )
+	// 		$sequence = null;
+
+	// 	$advert->title 		= $title;
+	// 	$advert->sequence 	= $sequence;
+	// 	$advert->url 		= $url;
+	// 	$advert->type 		= $type;
+	// 	if( !$advert->save() )
+	// 		return Response::json( BiaoException::$databaseErr );
+	// 	return Response::json( BiaoException::$ok );
+	// }
 	public function createAndEdit()
 	{	
 		if( Input::has( 'advert_id') )
@@ -31,21 +82,15 @@ class AdvertController extends Basecontroller{
 		}else{
 			$advert = new Advertisement;
 		}
-		$file 		= Input::file('image');
+		$file 		= Input::get('image');
 		$title 		= Input::get('title');
 		$sequence 	= Input::get('sequence');
-		// $sequence = 0;
 		$url 		= Input::get('url');
 		$type 		= Input::get('type');//1=微官网广告,2=微投票广告，3=微相册广告
-		// $type = 2;
-		//讲照片存入public目录
-		$path = public_path().'/upload/official/';
-		
 
 		$fullArr = array( $file,$title,$url,$type );
 		$littleArr = array( $title,$url,$type);
-		$dataPath = '/upload/official/';
-		$result = FileController::isFileUpload($advert,$file,$fullArr,$littleArr,$path,$dataPath);
+		$result = FileController::isImageUpload($advert,$file,$fullArr,$littleArr);
 		if( $result != 'true' )
 			return $result;
 
