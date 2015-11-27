30e9239fb35585425ef3d1311a54d9bc2986e93a
hyy <376246826@qq.com>
2015-11-27 13:32:51 +0800

    删除活动现场编辑模块

--- app/views/admin/pages/official/title/edit-title.blade.php
+++ app/views/admin/pages/official/title/edit-title.blade.php
@@ -7,69 +7,6 @@
 
 @section( 'edit-area' )
 
-<!-- 活动现场标题编辑 start -->
-<div class="edit-area-container edit-activity-title-area-wrap">
-
-<div class="edit-area-top clearfix">
-    <h3 class="edit-area-title">活动现场</h3>
-</div>
-
-<form action="" class="edit-area-form">
-
-<ul class="edit-area-body">
-    @if( isset( $title ) )
-    <li class="edit-area-row">
-        <label class="edit-area-label">标题1</label>
-        <input type="text" class="edit-area-input" value="{{$title->middle_title}}" readonly="readonly">
-        <!--
-        <span class="operation-wrap">
-            <button class="operation-edit operation-btn operation-blue-btn btn">编辑</button>
-            <button class="operation-modify operation-btn operation-blue-btn btn">确认</button>
-        </span>
-        -->
-    </li>
-    <li class="edit-area-row">
-        <label class="edit-area-label"></label>
-        <input type="text" class="edit-area-input" value="{{$title->middle_subtitle}}" readonly="readonly">
-        <!--
-        <span class="operation-wrap">
-            <button class="operation-edit operation-btn operation-blue-btn btn">编辑</button>
-            <button class="operation-modify operation-btn operation-blue-btn btn">确认</button>
-        </span>
-        -->
-    </li>
-    <li class="edit-area-row">
-        <label class="edit-area-label">标题2</label>
-        <input type="text" class="edit-area-input" value="{{$title->bottom_title}}" readonly="readonly">
-        <!--
-        <span class="operation-wrap">
-            <button class="operation-edit operation-btn operation-blue-btn btn">编辑</button>
-            <button class="operation-modify operation-btn operation-blue-btn btn">确认</button>
-        </span>
-        -->
-    </li>
-    <li class="edit-area-row">
-        <label class="edit-area-label"></label>
-        <input type="text" class="edit-area-input" value="{{$title->bottom_subtitle}}" readonly="readonly">
-        <!--
-        <span class="operation-wrap">
-            <button class="operation-edit operation-btn operation-blue-btn btn">编辑</button>
-            <button class="operation-modify operation-btn operation-blue-btn btn">确认</button>
-        </span>
-        -->
-
-    </li>
-    @endif
-
-
-    <!-- <input type="submit" class="operation-confirm btn" value="发布"> -->
-</ul>
-
-</form>
-
-</div>
-<!-- 活动现场标题编辑 end -->
-
 <!-- 软文干货标题编辑 start -->
 <div class="edit-area-container edit-resource-title-area-wrap">
 
