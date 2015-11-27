d495f19c13953e99b9a0a6cf7fad8315e8061905
hyy <376246826@qq.com>
2015-11-27 13:41:59 +0800

    不可编辑项会话

--- app/views/admin/pages/album/photo/add-photo.blade.php
+++ app/views/admin/pages/album/photo/add-photo.blade.php
@@ -25,7 +25,7 @@
 
     <li class="edit-area-row">
         <label class="edit-area-label">栏目</label>
-        <input type="text" class="edit-area-input" value="标王相册">
+        <input type="text" class="edit-area-input" value="标王相册" readonly="readonly">
     </li>
     
     <li class="edit-area-row">
